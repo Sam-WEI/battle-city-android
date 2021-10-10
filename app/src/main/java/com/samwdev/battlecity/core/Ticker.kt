8bf682f74e3cf3c5d2cef463c43d255bdfefb75c
Sam <shengkunwei@gmail.com>
2021-10-10 15:53:41 -0400

    improve ticker

--- app/src/main/java/com/samwdev/battlecity/core/Ticker.kt
+++ app/src/main/java/com/samwdev/battlecity/core/Ticker.kt
@@ -1,16 +1,12 @@
 package com.samwdev.battlecity.core
 
 import android.os.SystemClock
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
-import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlin.coroutines.CoroutineContext
 
-class Ticker : CoroutineScope, LifecycleObserver {
+class Ticker {
     companion object {
         private const val MAX_FPS = 5
         private const val FRAME_DUR = (1000f / MAX_FPS).toLong()
@@ -18,26 +14,16 @@ class Ticker : CoroutineScope, LifecycleObserver {
     private val _mutableStateFlow = MutableSharedFlow<Long>(0, onBufferOverflow = BufferOverflow.SUSPEND)
     val flow = _mutableStateFlow.asSharedFlow()
 
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.IO + Job()
-
-    fun start() {
-        launch {
-            var lastEmit = SystemClock.uptimeMillis()
-            while (true) {
-                val now = SystemClock.uptimeMillis()
-                val delta = now - lastEmit
-                if (delta > FRAME_DUR) {
-                    _mutableStateFlow.emit(delta)
-                    lastEmit = now
-                }
-                delay(FRAME_DUR)
+    suspend fun start() {
+        var lastEmit = SystemClock.uptimeMillis()
+        while (true) {
+            val now = SystemClock.uptimeMillis()
+            val delta = now - lastEmit
+            if (delta > FRAME_DUR) {
+                _mutableStateFlow.emit(delta)
+                lastEmit = now
             }
+            delay(FRAME_DUR)
         }
-    }
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-    fun stop() {
-        coroutineContext[Job]?.cancel()
     }
 }
