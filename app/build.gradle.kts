b7a474159222d117bb6342a53794b7d302388431
Sam <shengkunwei@gmail.com>
2021-10-09 23:59:33 -0400

    update gradles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,17 +49,17 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.compose.ui:ui:${Library.composeVersion}")
+    implementation(Library.ANDROIDX_CORE)
+    implementation(Library.ANDROIDX_APPCOMPAT)
+    implementation(Library.MATERIAL)
+    implementation(Library.COMPOSE_UI)
     implementation("androidx.compose.material:material:${Library.composeVersion}")
     implementation("androidx.compose.ui:ui-tooling-preview:${Library.composeVersion}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.1")
-    testImplementation("junit:junit:4.+")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    testImplementation(Library.JUNIT)
+    androidTestImplementation(Library.ANDROIDX_TEST_JUNIT)
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${Library.composeVersion}")
+    androidTestImplementation(Library.COMPOSE_UI_TEST_JUNIT)
     debugImplementation("androidx.compose.ui:ui-tooling:${Library.composeVersion}")
 }
