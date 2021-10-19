d07a61a2ee96b54e3be890a3959f411758191a69
Sam <shengkunwei@gmail.com>
2021-10-18 23:52:25 -0400

    modifier fix

--- app/src/main/java/com/samwdev/battlecity/components/joyStick.kt
+++ app/src/main/java/com/samwdev/battlecity/components/joyStick.kt
@@ -18,14 +18,13 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlin.math.*
 
-private val joyStickBg = Color.Gray
 private val joyStickBgBrush = Brush.radialGradient(colors = listOf(Color.Gray, Color.LightGray))
 
 @Composable
 fun JoyStick(modifier: Modifier = Modifier, onChange: (Offset) -> Unit) {
     val offset = remember { Animatable(Offset.Zero, Offset.VectorConverter) }
     Canvas(
-        modifier = Modifier
+        modifier = modifier
             .aspectRatio(1f)
             .pointerInput(Unit) {
                 val center = Offset(size.width / 2f, size.height / 2f)
@@ -70,14 +69,4 @@ fun JoyStick(modifier: Modifier = Modifier, onChange: (Offset) -> Unit) {
             radius = side / 6f,
         )
     }
-}
-
-private fun Offset.canvasPosition(side: Float): Offset {
-    val mid = side / 2f
-    return Offset(x - mid, y - mid)
-}
-
-private fun Offset.calcPositionPercentage(side: Float): Offset {
-    val mid = side / 2f
-    return Offset((x - mid) / mid, (y - mid) / mid)
 }
