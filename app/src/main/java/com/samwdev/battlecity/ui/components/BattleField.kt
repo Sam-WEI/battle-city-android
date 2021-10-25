add5a6355bedeb3bf95052962d9a2d386127c7c3
Sam <shengkunwei@gmail.com>
2021-10-24 23:07:56 -0400

    controller controls a box

--- app/src/main/java/com/samwdev/battlecity/ui/components/BattleField.kt
+++ app/src/main/java/com/samwdev/battlecity/ui/components/BattleField.kt
@@ -4,17 +4,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.withFrameMillis
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.samwdev.battlecity.core.Direction
-import com.samwdev.battlecity.core.TankModel
+import com.samwdev.battlecity.core.GameState
+import com.samwdev.battlecity.core.TankState
+import com.samwdev.battlecity.core.TanksViewModel
+import com.samwdev.battlecity.utils.logE
 import kotlinx.coroutines.launch
 
 @Composable
-fun BattleField(modifier: Modifier = Modifier) {
+fun BattleField(gameState: GameState, tanksViewModel: TanksViewModel, modifier: Modifier = Modifier) {
+    val tanks by tanksViewModel.tanks.collectAsState()
+    val tank by tanksViewModel.tank.collectAsState()
     LaunchedEffect(Unit) {
         launch {
             var last = withFrameMillis { it }
@@ -29,6 +32,7 @@ fun BattleField(modifier: Modifier = Modifier) {
         .fillMaxWidth()
         .aspectRatio(1f)
         .background(Color.Green)) {
-        Tank(tank = TankModel(x = 200, y = 200, direction = Direction.Down))
+        Tank(tank = tank)
     }
+
 }
--- app/src/main/java/com/samwdev/battlecity/ui/components/Tank.kt
+++ app/src/main/java/com/samwdev/battlecity/ui/components/Tank.kt
@@ -7,10 +7,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.samwdev.battlecity.core.TankModel
+import com.samwdev.battlecity.core.TankState
 
 @Composable
-fun Tank(tank: TankModel) {
+fun Tank(tank: TankState) {
     Canvas(modifier = Modifier.size(50.dp, 50.dp)) {
         drawRect(Color.Black, topLeft = Offset(tank.x.toFloat(), tank.y.toFloat()), size = size)
     }
