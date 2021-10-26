3a08da09a7cd68005775c28c41a2bd397e64b894
Sam <shengkunwei@gmail.com>
2021-10-26 00:11:12 -0400

    controllerState in Controller

--- app/src/main/java/com/samwdev/battlecity/MainActivity.kt
+++ app/src/main/java/com/samwdev/battlecity/MainActivity.kt
@@ -4,21 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.samwdev.battlecity.core.TanksViewModel
+import androidx.compose.ui.unit.sp
 import com.samwdev.battlecity.core.rememberGameState
 import com.samwdev.battlecity.ui.components.BattleField
 import com.samwdev.battlecity.ui.components.Controller
 import com.samwdev.battlecity.ui.components.rememberControllerState
 import com.samwdev.battlecity.ui.theme.BattleCityTheme
-import kotlin.math.roundToInt
 
 @ExperimentalAnimationApi
 @ExperimentalMaterialApi
@@ -26,25 +29,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val tanksViewModel = TanksViewModel()
-            val gameState = rememberGameState(tanksViewModel)
             val scope = rememberCoroutineScope()
+            val gameState = rememberGameState()
 
             Column(modifier = Modifier.fillMaxSize()) {
-                val controllerState = rememberControllerState()
-                BattleField(gameState = gameState, tanksViewModel = tanksViewModel, modifier = Modifier.fillMaxWidth())
+                BattleField(gameState = gameState, modifier = Modifier.fillMaxWidth())
                 Controller(
                     modifier = Modifier
                         .padding(30.dp)
                         .fillMaxWidth(),
-                    onSteer = { offset ->
-                        tanksViewModel.updateTank((5 * offset.x).roundToInt(), (5 * offset.y).roundToInt())
-                        controllerState.setCurrentInput(offset = offset)
-                    },
-                    onFire = {  },
+                    controllerState = gameState.controllerState,
                 )
 
-                Text(text = "ctrler State: ${controllerState.direction}", color = Color.Gray)
+                Text(text = "direction: ${gameState.controllerState.direction}", color = Color.Gray, fontSize = 30.sp)
             }
         }
     }
