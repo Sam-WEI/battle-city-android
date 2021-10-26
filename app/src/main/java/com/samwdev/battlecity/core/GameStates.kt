a7f41246edeefe73e30b3519a82a553aa11746c7
Sam <shengkunwei@gmail.com>
2021-10-26 01:25:32 -0400

    refactor ticker

--- app/src/main/java/com/samwdev/battlecity/core/GameStates.kt
+++ app/src/main/java/com/samwdev/battlecity/core/GameStates.kt
@@ -6,16 +6,19 @@ import com.samwdev.battlecity.ui.components.rememberControllerState
 
 @Composable
 fun rememberGameState(
+    tickState: TickState = rememberTickState(),
     tank: TankState = remember { TankState(0, 0) },
     controllerState: ControllerState = rememberControllerState(),
 ) = remember {
     GameState(
+        tickState = tickState,
         controllerState = controllerState,
         tankState = tank
     )
 }
 
 class GameState(
+    val tickState: TickState,
     val controllerState: ControllerState,
     val tanks: Map<String, TankState> = mapOf(),
     val tankState: TankState,
