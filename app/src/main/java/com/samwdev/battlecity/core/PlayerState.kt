fc4df01aeeb6ea5baed07c3815e49c94f91fb6a6
Sam <shengkunwei@gmail.com>
2021-10-27 00:30:08 -0400

    player state

---
+++ app/src/main/java/com/samwdev/battlecity/core/PlayerState.kt
@@ -0,0 +1,19 @@
+package com.samwdev.battlecity.core
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class PlayerState(
+    val name: String = "player_1",
+    lives: Int = 3,
+    score: Int = 0,
+    tankId: Long,
+) {
+    var lives by mutableStateOf(lives)
+        private set
+    var score by mutableStateOf(score)
+        private set
+    var tankId by mutableStateOf(tankId)
+        private set
+}
