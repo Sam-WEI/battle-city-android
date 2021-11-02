201526b38096d09aff80d3fa2beb07f553a61ef0
Sam <shengkunwei@gmail.com>
2021-11-02 17:15:46 -0400

    FPS counter

--- app/src/main/java/com/samwdev/battlecity/ui/components/BattleField.kt
+++ app/src/main/java/com/samwdev/battlecity/ui/components/BattleField.kt
@@ -6,21 +6,36 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import com.samwdev.battlecity.core.*
+import kotlin.math.roundToInt
 
 @Composable
 fun BattleField(battleState: BattleState, modifier: Modifier = Modifier) {
     LaunchedEffect(Unit) {
         battleState.start()
     }
-    Box(modifier = Modifier
+    Box(modifier = modifier
         .fillMaxWidth()
         .aspectRatio(1f)
-        .background(Color.Green)) {
-        Text(text = "tick: ${battleState.tickState.uptimeMillis}. delta: ${battleState.tickState.delta}.")
+        .background(Color.Black)) {
+        Text(
+            text = "FPS: ${(1000f / battleState.tickState.delta).roundToInt()}",
+            color = Color.Green,
+            fontSize = 14.sp,
+            modifier = Modifier.align(Alignment.TopEnd)
+        )
         Tank(tank = battleState.tankState)
-    }
 
+    }
+}
+
+@Composable
+fun MapBackground() {
+    Box() {
+
+    }
 }
