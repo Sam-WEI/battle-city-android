bfcc45e9cf57cc300b6388ffdcff5a70469b9b1b
Sam <shengkunwei@gmail.com>
2021-11-02 21:40:42 -0400

    fix brick steel elements

--- app/src/main/java/com/samwdev/battlecity/entity/Map.kt
+++ app/src/main/java/com/samwdev/battlecity/entity/Map.kt
@@ -1,3 +1,41 @@
 package com.samwdev.battlecity.entity
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
+import com.samwdev.battlecity.ui.components.mu
+
 const val MAP_BLOCK_COUNT = 13
+
+sealed class MapElement(open val index: Int, private val granularity: Int = 1) {
+    val offset: IntOffset
+        get() {
+            val row = index / (granularity * MAP_BLOCK_COUNT)
+            val col = index % (granularity * MAP_BLOCK_COUNT)
+            return IntOffset(col, row)
+        }
+
+    val offsetInMapUnit: Offset get() {
+        val (col, row) = offset
+        val rowF = row / granularity.toFloat()
+        val colF = col / granularity.toFloat()
+        return Offset(colF, rowF)
+    }
+
+    val size: Dp @Composable get() = 1.mu / granularity.toFloat()
+}
+
+data class BrickElement(override val index: Int) : MapElement(index, 4) {
+
+}
+
+data class SteelElement(override val index: Int) : MapElement(index, 2) {
+
+}
+
+data class TreeElement(override val index: Int) : MapElement(index)
+data class WaterElement(override val index: Int) : MapElement(index)
+data class IceElement(override val index: Int) : MapElement(index)
+data class EagleElement(override val index: Int) : MapElement(index)
--- app/src/main/java/com/samwdev/battlecity/ui/components/BattleField.kt
+++ app/src/main/java/com/samwdev/battlecity/ui/components/BattleField.kt
@@ -7,13 +7,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.samwdev.battlecity.core.*
+import com.samwdev.battlecity.entity.BrickElement
 import com.samwdev.battlecity.entity.MAP_BLOCK_COUNT
-import kotlin.math.min
+import com.samwdev.battlecity.entity.SteelElement
+import com.samwdev.battlecity.entity.TreeElement
 import kotlin.math.roundToInt
 
 @Composable
@@ -24,12 +24,23 @@ fun BattleField(
     LaunchedEffect(Unit) {
         battleState.start()
     }
+    val brickElements = remember(battleState.mapState.bricks) {
+        battleState.mapState.bricks.map { BrickElement(it) }
+    }
+    val steelElements = remember(battleState.mapState.steels) {
+        battleState.mapState.steels.map { SteelElement(it) }
+    }
+    val treeElements = remember(battleState.mapState.trees) {
+        battleState.mapState.trees.map { TreeElement(it) }
+    }
     Map(modifier = modifier
         .fillMaxWidth()
         .aspectRatio(1f)
-        .background(Color.Black)) {
-        TreeLayer(trees = battleState.mapState.trees)
-        BrickLayer(bricks = battleState.mapState.bricks)
+        .background(Color.Black)
+    ) {
+        TreeLayer(trees = treeElements)
+        BrickLayer(bricks = brickElements)
+        SteelLayer(steel = steelElements)
         Text(
             text = "FPS: ${(1000f / battleState.tickState.delta).roundToInt()}",
             color = Color.Green,
@@ -60,31 +71,48 @@ private fun Map(
 val Int.mu: Dp @Composable get() = Dp(LocalMapUnit.current * this)
 val Float.mu: Dp @Composable get() = Dp(LocalMapUnit.current * this)
 
-@Composable
-fun ProvideGameUiUnit(
-    content: @Composable () -> Unit,
-) {
-    val mapSide = with (LocalContext.current.resources.displayMetrics) {
-        min(widthPixels, heightPixels)
-    }
-    CompositionLocalProvider(LocalMapUnit provides 5f, content = content)
-}
-
-val LocalMapUnit = staticCompositionLocalOf<Float> {
+private val LocalMapUnit = staticCompositionLocalOf<Float> {
     error("Not in Map composable or its child composable.")
 }
 
 @Composable
-fun BrickLayer(bricks: List<Int>) {
-    Box {
-        Text(text = bricks.joinToString(), color = Color.White)
+fun BrickLayer(bricks: List<BrickElement>) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        bricks.take(15000).forEach { br ->
+            val (x, y) = br.offsetInMapUnit
+            Box(
+                modifier = Modifier.offset(x.mu, y.mu)
+                    .size(br.size)
+                    .background(Color.Red)
+            )
+        }
     }
 }
 
 @Composable
-fun TreeLayer(trees: List<Int>) {
-    Box {
-        Text("TREES", color = Color.White)
-        Text(text = trees.joinToString(), color = Color.White)
+fun SteelLayer(steel: List<SteelElement>) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        steel.forEach { el ->
+            val (x, y) = el.offsetInMapUnit
+            Box(
+                modifier = Modifier.offset(x.mu, y.mu)
+                    .size(el.size)
+                    .background(Color.Gray)
+            )
+        }
+    }
+}
+
+@Composable
+fun TreeLayer(trees: List<TreeElement>) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        trees.forEach { el ->
+            val (x, y) = el.offsetInMapUnit
+            Box(
+                modifier = Modifier.offset(x.mu, y.mu)
+                    .size(el.size)
+                    .background(Color.Green)
+            )
+        }
     }
 }
