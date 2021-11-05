e484731f9afa5201bc9d3d3ab98983509e1344ad
Sam <shengkunwei@gmail.com>
2021-11-05 11:39:11 -0400

    tank controller

--- app/src/main/java/com/samwdev/battlecity/core/TankController.kt
+++ app/src/main/java/com/samwdev/battlecity/core/TankController.kt
@@ -5,7 +5,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
 class TankController(
-    private val tankState: TankState
+    private val tankState: TankState,
+    private val handheldControllerState: HandheldControllerState,
 ) {
     private var _tankId: Int by mutableStateOf(-1)
 
@@ -13,8 +14,22 @@ class TankController(
         _tankId = id
     }
 
-    fun onTick() {
-        val tank = tankState.tanks[_tankId]
-        // todo check user input
+    fun onTick(tick: Tick) {
+        val tank = tankState.getTank(_tankId) ?: return
+        val move = tank.speed * tick.delta
+        when (handheldControllerState.direction) {
+            Direction.Left -> tank.x -= move
+            Direction.Right -> tank.x += move
+            Direction.Up -> tank.y -= move
+            Direction.Down -> tank.y += move
+            Direction.Unspecified -> {}
+        }
+        if (handheldControllerState.direction != Direction.Unspecified) {
+            tank.direction = handheldControllerState.direction
+        }
+
+        if (handheldControllerState.firePressed) {
+
+        }
     }
 }
