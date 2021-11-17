ac2a91de1761a7fe6e9b583a2c203978442579bb
Sam Wei <shengkunwei@gmail.com>
2021-11-17 01:40:21 -0500

    spawn blink

--- app/src/main/java/com/samwdev/battlecity/core/PixelCanvas.kt
+++ app/src/main/java/com/samwdev/battlecity/core/PixelCanvas.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.scale
 import com.samwdev.battlecity.ui.components.LocalMapPixelDp
 import com.samwdev.battlecity.ui.components.mpx2dp
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * This canvas allows you to draw in original game pixel
@@ -41,5 +43,21 @@ fun PixelCanvas(
 class PixelDrawScope(private val drawScope: DrawScope) : DrawScope by drawScope {
     fun drawPixelPoint(color: Color, topLeft: Offset) {
         drawRect(color = color, topLeft = topLeft, size = Size(1f, 1f))
+    }
+
+    fun drawVerticalLine(color: Color, topLeft: Offset, length: MapPixel) {
+        drawRect(color = color, topLeft = topLeft, size = Size(1f, length))
+    }
+
+    fun drawHorizontalLine(color: Color, topLeft: Offset, length: MapPixel) {
+        drawRect(color = color, topLeft = topLeft, size = Size(length, 1f))
+    }
+
+    fun drawDiagonalLine(color: Color, end1: Offset, end2: Offset) {
+        val l = min(end1.x.toInt(), end2.x.toInt())
+        val r = max(end1.x.toInt(), end2.x.toInt())
+        val t = min(end1.y.toInt(), end2.y.toInt())
+        val b = max(end1.y.toInt(), end2.y.toInt())
+
     }
 }
--- app/src/main/java/com/samwdev/battlecity/ui/components/SpawnBlink.kt
+++ app/src/main/java/com/samwdev/battlecity/ui/components/SpawnBlink.kt
@@ -4,26 +4,69 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.samwdev.battlecity.core.PixelCanvas
+import com.samwdev.battlecity.core.PixelDrawScope
+import com.samwdev.battlecity.core.TickState
 import com.samwdev.battlecity.core.grid2mpx
 import com.samwdev.battlecity.ui.theme.BattleCityTheme
 
 @Composable
-fun SpawnBlink() {
-
+fun SpawnBlink(tickState: TickState, topLeft: Offset) {
+    Framer(
+        tickState = tickState, framesDef = listOf(100, 100, 100, 100),
+        infinite = true,
+    ) {
+        val index = LocalFramer.current
+        SpawnBlinkIndex(index = index, topLeft = topLeft)
+    }
 }
 
 @Composable
-fun SpawnBlinkSmall(topLeft: Offset) {
+fun SpawnBlinkIndex(index: Int, topLeft: Offset) {
     PixelCanvas(
         widthInMapPixel = 1f.grid2mpx,
         heightInMapPixel = 1f.grid2mpx,
         topLeftInMapPixel = topLeft,
     ) {
-
+        when (index) {
+            0 -> spawnBlink1()
+            1 -> spawnBlink2()
+            2 -> spawnBlink3()
+            3 -> spawnBlink4()
+        }
     }
+}
+
+private fun PixelDrawScope.spawnBlink1() {
+    drawHorizontalLine(color = Color.White, topLeft = Offset(3f, 7f), length = 9f)
+    drawVerticalLine(color = Color.White, topLeft = Offset(7f, 3f), length = 9f)
+    drawRect(color = Color.White, topLeft = Offset(6f, 6f), size = Size(3f, 3f))
+}
+
+private fun PixelDrawScope.spawnBlink2() {
+    drawHorizontalLine(color = Color.White, topLeft = Offset(2f, 7f), length = 11f)
+    drawVerticalLine(color = Color.White, topLeft = Offset(7f, 2f), length = 11f)
+    drawRect(color = Color.White, topLeft = Offset(6f, 5f), size = Size(3f, 5f))
+    drawRect(color = Color.White, topLeft = Offset(5f, 6f), size = Size(5f, 3f))
+}
+
+private fun PixelDrawScope.spawnBlink3() {
+    drawHorizontalLine(color = Color.White, topLeft = Offset(1f, 7f), length = 13f)
+    drawVerticalLine(color = Color.White, topLeft = Offset(7f, 1f), length = 13f)
+    drawRect(color = Color.White, topLeft = Offset(6f, 4f), size = Size(3f, 7f))
+    drawRect(color = Color.White, topLeft = Offset(4f, 6f), size = Size(7f, 3f))
+}
+
+private fun PixelDrawScope.spawnBlink4() {
+    drawHorizontalLine(color = Color.White, topLeft = Offset(0f, 7f), length = 15f)
+    drawVerticalLine(color = Color.White, topLeft = Offset(7f, 0f), length = 15f)
+    drawRect(color = Color.White, topLeft = Offset(6f, 3f), size = Size(3f, 9f))
+    drawRect(color = Color.White, topLeft = Offset(3f, 6f), size = Size(9f, 3f))
+    drawRect(color = Color.White, topLeft = Offset(5f, 5f), size = Size(5f, 5f))
 }
 
 @Preview
@@ -34,7 +77,9 @@ fun SpawnBlinkPreview() {
             modifier = Modifier.size(500.dp),
             sideBlockCount = 4,
         ) {
-            SpawnBlink()
+            for (i in 0..3) {
+                SpawnBlinkIndex(index = i, topLeft = Offset(0f, i.toFloat().grid2mpx))
+            }
         }
     }
 }
