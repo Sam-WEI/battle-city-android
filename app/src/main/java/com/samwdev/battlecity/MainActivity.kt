b26c5abfb2654d1de6d00399e45bf9075cbe2092
Sam Wei <shengkunwei@gmail.com>
2021-11-18 10:53:37 -0500

    sound

--- app/src/main/java/com/samwdev/battlecity/MainActivity.kt
+++ app/src/main/java/com/samwdev/battlecity/MainActivity.kt
@@ -7,19 +7,40 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.samwdev.battlecity.core.SoundPlayer
 import com.samwdev.battlecity.ui.components.BattleScreen
 import com.samwdev.battlecity.ui.theme.BattleCityTheme
 import com.samwdev.battlecity.utils.MapParser
+import kotlinx.coroutines.launch
 
 @ExperimentalAnimationApi
 @ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
+    private lateinit var soundPlayer: SoundPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        soundPlayer = SoundPlayer()
+        lifecycleScope.launchWhenStarted { soundPlayer.init(this@MainActivity) }
+        lifecycleScope.launchWhenResumed { soundPlayer.resume() }
         setContent {
             BattleCityTheme {
                 BattleScreen(MapParser.parseJson(14))
             }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        lifecycleScope.launch {
+            soundPlayer.pause()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        lifecycleScope.launch {
+            soundPlayer.release()
         }
     }
 }
---
+++ app/src/main/java/com/samwdev/battlecity/core/SoundPlayer.kt
@@ -0,0 +1,57 @@
+package com.samwdev.battlecity.core
+
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.SoundPool
+import androidx.annotation.RawRes
+import com.samwdev.battlecity.R
+
+class SoundPlayer {
+    private val soundIdMap = mutableMapOf<SoundEffect, Int>()
+    private lateinit var soundPool: SoundPool
+
+    suspend fun init(context: Context) {
+        soundPool = SoundPool.Builder()
+            .setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .build()
+            )
+            .setMaxStreams(5)
+            .build()
+
+        SoundEffect.values().forEach {
+            val soundId = soundPool.load(context.applicationContext, it.resId, 1)
+            soundIdMap[it] = soundId
+        }
+    }
+
+    suspend fun play(soundEffect: SoundEffect) {
+        val streamId = soundPool.play(soundIdMap.getValue(soundEffect), 1f, 1f, 0, 0, 1f)
+    }
+
+    suspend fun pause() {
+        soundPool.autoPause()
+    }
+
+    suspend fun resume() {
+        soundPool.autoResume()
+    }
+
+    suspend fun release() {
+        soundPool.release()
+    }
+}
+
+enum class SoundEffect(@RawRes val resId: Int) {
+    BulletHit1(R.raw.bullet_hit_1),
+    BulletHit2(R.raw.bullet_hit_2),
+    BulletShot(R.raw.bullet_shot),
+    Explosion1(R.raw.explosion_1),
+    Explosion2(R.raw.explosion_2),
+    GameOver(R.raw.game_over),
+    Pause(R.raw.pause),
+    PowerUpAppear(R.raw.powerup_appear),
+    PowerUpPick(R.raw.powerup_pick),
+}
