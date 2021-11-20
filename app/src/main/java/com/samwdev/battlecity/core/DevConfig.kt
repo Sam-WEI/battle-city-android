9a28d1da43c085f64f626c72d3fdf9600c65951b
Sam Wei <shengkunwei@gmail.com>
2021-11-19 21:56:39 -0500

    dev config control panel

--- app/src/main/java/com/samwdev/battlecity/core/DevConfig.kt
+++
@@ -1,3 +0,0 @@
-package com.samwdev.battlecity.core
-
-const val SHOW_BRICK_INDEX: Boolean = false
--- app/src/main/java/com/samwdev/battlecity/ui/components/BattleScreen.kt
+++ app/src/main/java/com/samwdev/battlecity/ui/components/BattleScreen.kt
@@ -1,45 +1,66 @@
 package com.samwdev.battlecity.ui.components
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.samwdev.battlecity.core.BattleState
-import com.samwdev.battlecity.core.HandheldController
-import com.samwdev.battlecity.core.rememberBattleState
+import com.samwdev.battlecity.core.*
 import com.samwdev.battlecity.entity.StageConfigJson
+import com.samwdev.battlecity.ui.theme.BattleCityTheme
 
 @Composable
 fun BattleScreen(stageConfigJson: StageConfigJson) {
     val battleState: BattleState = rememberBattleState(stageConfigJson = stageConfigJson)
+    var debugConfig: DebugConfig by remember { mutableStateOf(DebugConfig(showFps = true)) }
 
     LaunchedEffect(Unit) {
         battleState.startBattle()
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        BattleField(
-            modifier = Modifier.fillMaxWidth(),
-            battleState = battleState,
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            CompositionLocalProvider(LocalDebugConfig provides debugConfig) {
+                BattleField(
+                    modifier = Modifier.fillMaxWidth(),
+                    battleState = battleState,
+                )
+            }
+            Box(modifier = Modifier.fillMaxSize()) {
+                HandheldController(
+                    modifier = Modifier
+                        .padding(horizontal = 30.dp, vertical = 60.dp)
+                        .fillMaxWidth(),
+                    handheldControllerState = battleState.handheldControllerState,
+                )
+            }
+        }
+
+        DebugConfigControlToggle(
+            modifier = Modifier.fillMaxWidth().height(400.dp).align(Alignment.BottomEnd),
+            debugConfig = debugConfig,
+            onConfigChange = { debugConfig = it }
         )
-        Box {
-            HandheldController(
-                modifier = Modifier
-                    .padding(30.dp)
-                    .fillMaxWidth(),
-                handheldControllerState = battleState.handheldControllerState,
-            )
-            Text(
-                text = "${battleState.handheldControllerState.direction}",
-                color = Color.Green,
-                fontSize = 20.sp,
-                modifier = Modifier.align(Alignment.TopStart)
-            )
+    }
+}
+
+val LocalDebugConfig = staticCompositionLocalOf { DebugConfig() }
+
+@Preview
+@Composable
+private fun BattleStatePreview() {
+    BattleCityTheme {
+        Column(modifier = Modifier.size(500.dp, 500.dp)) {
+            Box {
+                HandheldController(
+                    modifier = Modifier
+                        .padding(horizontal = 30.dp, vertical = 60.dp)
+                        .fillMaxWidth(),
+                    handheldControllerState = HandheldControllerState(),
+                )
+            }
+            DebugConfigControlPanel(debugConfig = DebugConfig(), onConfigChange = {})
         }
     }
 }
