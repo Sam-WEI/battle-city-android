2a329a21c726b14988ecb148d788e9614302394f
Sam Wei <shengkunwei@gmail.com>
2021-11-20 14:19:57 -0500

    readme

---
+++ app/src/main/java/com/samwdev/battlecity/core/ScoreState.kt
@@ -0,0 +1,10 @@
+package com.samwdev.battlecity.core
+
+
+
+val BotKillingScore = mapOf(
+    TankLevel.Level1 to 100,
+    TankLevel.Level2 to 200,
+    TankLevel.Level3 to 300,
+    TankLevel.Level4 to 400,
+)
