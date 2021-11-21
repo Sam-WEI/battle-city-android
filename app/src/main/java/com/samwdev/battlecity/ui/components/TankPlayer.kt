0bacd3a2f82a2dc20d18eeba86bc9231011ff5ee
Sam Wei <shengkunwei@gmail.com>
2021-11-21 10:40:47 -0500

    TankTreadsPreview

--- app/src/main/java/com/samwdev/battlecity/ui/components/TankPlayer.kt
+++ app/src/main/java/com/samwdev/battlecity/ui/components/TankPlayer.kt
@@ -1,9 +1,19 @@
 package com.samwdev.battlecity.ui.components
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.samwdev.battlecity.core.Tank
+import com.samwdev.battlecity.core.TankLevel
+import com.samwdev.battlecity.core.TankSide
+import com.samwdev.battlecity.core.grid2mpx
+import com.samwdev.battlecity.ui.theme.BattleCityTheme
 
 
 fun PixelDrawScope.drawPlayerTankLevel1(treadPattern: Int) {
@@ -124,4 +134,28 @@ fun PixelDrawScope.drawPlayerTankLevel1(treadPattern: Int) {
         topLeft = Offset(7f, 2f),
         length = 5f,
     )
+}
+
+@Preview
+@Composable
+private fun PlayerTankPreview() {
+    BattleCityTheme {
+        Map(modifier = Modifier.size(500.dp), sideBlockCount = 8) {
+            TankTreadsPreview(
+                tank = Tank(
+                    id = 0,
+                    x = 0f.grid2mpx, y = 0f.grid2mpx,
+                    level = TankLevel.Level1,
+                    side = TankSide.Player,
+                    hp = 1,
+                ),
+            )
+        }
+    }
+}
+
+@Composable
+fun TankTreadsPreview(tank: Tank) {
+    TankWithTreadPattern(tank = tank, treadPattern = 0)
+    TankWithTreadPattern(tank = tank.copy(x = tank.x + 1f.grid2mpx), treadPattern = 1)
 }
