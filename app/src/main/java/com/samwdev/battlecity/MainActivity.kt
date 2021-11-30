84d99091b01beec27e0a1f1acb5a4dfcc22b1daa
Sam Wei <shengkunwei@gmail.com>
2021-11-30 00:36:07 -0500

    full screen

--- app/src/main/java/com/samwdev/battlecity/MainActivity.kt
+++ app/src/main/java/com/samwdev/battlecity/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.samwdev.battlecity
 
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.lifecycleScope
 import com.samwdev.battlecity.core.SoundPlayer
 import com.samwdev.battlecity.ui.components.BattleCityApp
@@ -41,6 +46,17 @@ class MainActivity : ComponentActivity() {
         super.onStop()
         lifecycleScope.launch {
             soundPlayer.release()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+            WindowCompat.getInsetsController(window, window.decorView)?.let {
+                it.hide(WindowInsetsCompat.Type.systemBars())
+                it.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
         }
     }
 }
