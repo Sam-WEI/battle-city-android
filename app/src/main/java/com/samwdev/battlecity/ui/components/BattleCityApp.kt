dc4363dac609de653f8b592d0ee64dcd1765211c
Sam Wei <shengkunwei@gmail.com>
2021-11-30 00:20:10 -0500

    compose nav

---
+++ app/src/main/java/com/samwdev/battlecity/ui/components/BattleCityApp.kt
@@ -0,0 +1,39 @@
+package com.samwdev.battlecity.ui.components
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.samwdev.battlecity.core.Route
+import com.samwdev.battlecity.core.rememberAppState
+import com.samwdev.battlecity.ui.theme.BattleCityTheme
+import com.samwdev.battlecity.utils.MapParser
+import kotlin.random.Random
+
+
+@ExperimentalAnimationApi
+@ExperimentalMaterialApi
+@Composable
+fun BattleCityApp() {
+    val appState = rememberAppState()
+    BattleCityTheme {
+        var stageName = Random.nextInt(1, 36).toString()
+        stageName = 2.toString()
+
+        val json = MapParser.readJsonFile(LocalContext.current, stageName)
+        val stageConfig = MapParser.parse(json)
+        NavHost(
+            navController = appState.navController,
+            startDestination = Route.BattleScreen,
+        ) {
+            composable(Route.Home) {
+
+            }
+            composable(Route.BattleScreen) {
+                BattleScreen(stageConfig)
+            }
+        }
+    }
+}
