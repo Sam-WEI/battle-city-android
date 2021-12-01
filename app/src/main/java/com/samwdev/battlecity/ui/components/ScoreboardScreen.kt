da161bde903f0847fe625701b8d60b33e7a7bda7
Sam Wei <shengkunwei@gmail.com>
2021-11-30 23:06:03 -0500

    game over screen

---
+++ app/src/main/java/com/samwdev/battlecity/ui/components/ScoreboardScreen.kt
@@ -0,0 +1,14 @@
+package com.samwdev.battlecity.ui.components
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+
+@Composable
+fun ScoreboardScreen() {
+    BrickTitle(
+        texts = arrayOf("GAME", "OVER"),
+        modifier = Modifier.fillMaxSize().scale(0.5f),
+    )
+}
