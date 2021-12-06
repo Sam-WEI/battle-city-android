50e62db91f3b05cc065846024e1cf027be920e4a
Sam Wei <shengkunwei@gmail.com>
2021-12-06 00:48:01 -0500

    font for on screen score

--- app/src/main/java/com/samwdev/battlecity/ui/components/OnScreenScore.kt
+++ app/src/main/java/com/samwdev/battlecity/ui/components/OnScreenScore.kt
@@ -1,19 +1,41 @@
 package com.samwdev.battlecity.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.sp
 import com.samwdev.battlecity.core.OnScreenScore
+import com.samwdev.battlecity.core.grid2mpx
+import com.samwdev.battlecity.core.toMpx
+import com.samwdev.battlecity.ui.theme.CiiFont
 
 @Composable
 fun OnScreenScore(onScreenScore: OnScreenScore) {
-    PixelCanvas(topLeftInMapPixel = onScreenScore.offset) {
-        drawText(
+    val charHeightDp = 18f
+    val fontScale = LocalDensity.current.fontScale
+    val charHeightSp = (charHeightDp / fontScale).sp
+    Box(
+        modifier = Modifier.offset(
+            onScreenScore.offset.x.mpx2dp,
+            onScreenScore.offset.y.mpx2dp
+        ).size(1f.grid2mpx.mpx2dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
             text = onScreenScore.score.toString(),
             color = Color.White,
-            offset = Offset(0f, 12f),
-            fontSize = 2.sp
+            fontSize = charHeightSp,
+            letterSpacing = 1.sp,
+            fontFamily = CiiFont,
         )
     }
 }
