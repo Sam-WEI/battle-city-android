bdb8cacd6fe1a8d05e3300136ff6e4b464c9393a
Sam Wei <shengkunwei@gmail.com>
2022-11-30 23:33:32 -0600

    remove com.google.android.material:material:1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     implementation(composeBom)
     androidTestImplementation(composeBom)
     implementation("androidx.compose.material3:material3")
-    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
